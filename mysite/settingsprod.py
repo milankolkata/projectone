27eb0a5178c3378083df23880a2b381c5d39f845
Gaurav <milankolkata1977@gmail.com>
2024-09-03 20:29:28 +0530

    changed allowed hots in settingsprod

--- mysite/settingsprod.py
+++ mysite/settingsprod.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milankolkata.com']
 
 
 # Application definition
